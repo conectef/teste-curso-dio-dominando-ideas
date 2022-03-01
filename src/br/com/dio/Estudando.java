1c4cc370b4a5dd9f400bcd75d56d2f187737e7ae
Flavio <conectef@gmail.com>
2022-03-01 18:10:00 -0300

    Estudos

---
+++ src/br/com/dio/Estudando.java
@@ -0,0 +1,21 @@
+package br.com.dio;
+
+public class Estudando {
+    public static void main(String[] args) {
+
+        int maca_mel = 20;
+        int Macaiba = 1;
+        int QtdProdutos = 22;
+        int CHAVES_ROSA = 28;
+        final int macaco = 5;
+
+        System.out.println(maca_mel);
+        System.out.println(Macaiba);
+        System.out.println(QtdProdutos);
+        System.out.println(CHAVES_ROSA);
+        System.out.println(macaco);
+    }
+
+
+}
+
